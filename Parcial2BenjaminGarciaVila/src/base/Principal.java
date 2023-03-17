bd21c2652acbbb05c5c2903fd0be9fa5aa7d1121
beni <wualas@gmail.com>
2023-03-17 11:16:24 +0100

    Menu principal

--- Parcial2BenjaminGarciaVila/src/base/Principal.java
+++ Parcial2BenjaminGarciaVila/src/base/Principal.java
@@ -1,10 +1,84 @@
+/**
+Desarrollar una aplicación en JAVA que permita gestionar los dispositivos móviles que
+vende una tienda de telefonía móvil.
+La aplicación permitirá llevar registro de todos los dispositivos de la tienda, teniendo en
+cuenta que cada dispositivo se especializa en Android o iOS.
+Ten en cuenta que, tal y como se muestra en el diagrama uml los dispositivos Android y los
+dispositivos iOS además de los métodos específicos de cada uno, incluirán los
+constructores y métodos de la superclase y los métodos get, set y toString
+correspondientes. Asimismo, se ha de tener en cuenta que el modelo se considerará un
+atributo clave por lo que no admitirá repetidos.
+Los métodos indicados en el diagrama uml se limitarán a mostrar un mensaje por pantalla,
+mensaje que será distinto y personalizado según se trate de un Smartphone Android o un
+Smartphone iOS tal y como se indica a continuación:
+Smartphone Android:
+llamar(): “LLamada desde un Smartphone Android”
+colgar(): “Deslice para colgar la llamada desde un Smartphone Android”.
+buscar(): “Abrir agenda desde un Smartphone Android”
+tomarFotografia(): “Accediendo a la cámara desde Smartphone Android”.
+Smartphone iOS:
+llamar(): “LLamada de voz desde un Smartphone iOS”
+colgar(): “Pulsación larga para colgar la llamada desde un Smartphone iOS”.
+buscar(): “Abrir agenda de contactos desde un Smartphone iOS”
+interactuarPorRedSocial(): “Accediendo a la redes sociales desde Smartphone iOS”.
+El método toString mostrará por pantalla toda la información de cada dispositivo incluyendo
+los campos/atributos así como la llamada a los métodos indicados anteriormente.
+Menú principal:
+1.- Submenú Smartphones Android
+2.- Submenú Smartphones iOS
+0.- Salir
+Submenú Smartphones Android
+1. Alta de dispositivo
+2. Mostrar listado de dispositivos
+3. —>>> apartado a añadir el día del examen presencial
+4. Volver al menú principal
+Submenú Smartphones iOS
+1. Alta de dispositivo
+2. Mostrar listado de dispositivos
+3. —>>> apartado a añadir el día del examen presencial
+4. Volver al menú principal
+*************************************************************************************
+ * @author Benjamin Garcia
+ * @version 1.0
+ * @date 21/03/2023
+ * DNI: 53301596Q
+ * email: benjamin.garcia@formacionchios.es
+*/
+
 package base;
 
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
+import utilidades.Menu;
+import datos.DispositivoAndroid;
+import datos.DispositivoIos;
+import datos.DispositivoMovil;
+
+
+
+
+
 public class Principal {
+//VARIABLES GLOBALES
+	static Scanner sc = new Scanner(System.in);
+	static ArrayList<DispositivoMovil> listadomoviles = new ArrayList<DispositivoMovil>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// TODO Auto-generated method stub
+	Menu.menuPrincipal();
+	sc.close();
 	}
 
+public static void AltaDispositivoAndroid() {}
+public static void mostrarListadoAndroid() {}
+public static void Examen() {}
+public static void AltaDispositivoIos() {}
+public static void mostrarListadoIos() {}
+public static void Examen2() {}
+	
+	
+	
+	
 }
--- Parcial2BenjaminGarciaVila/src/utilidades/Menu.java
+++ Parcial2BenjaminGarciaVila/src/utilidades/Menu.java
@@ -1,5 +1,121 @@
 package utilidades;
 
-public class Menu {
+import java.util.Scanner;
 
+import base.Principal;
+
+public class Menu {
+	//Variables globales
+	static Scanner sc = new Scanner(System.in);
+	
+	//Menu principal
+    public static void menuPrincipal() {
+    	final int tope=2;
+    	int opc;
+    	do {
+    		System.out.println("\n \n ===   MENU   === \n");
+    		System.out.println(" 1 - Submenu Smartphone Android");
+    		System.out.println(" 2 - Submenu Smartphone IOS");
+    		System.out.println(" 0 - Salir");
+    		opc = sc.nextInt();
+    		if (opc <0 || opc > tope) {
+    			System.out.println("Error opcion invalida. Intentelo de nuevo");
+    		}
+    	}while (opc <0 || opc >tope);
+    	switchMenuPrincipal(opc);
+    }
+  //Switch menu principal
+    public static void switchMenuPrincipal(int opcion) {
+    	switch (opcion) { 
+    	case 1:
+    		submenuAndroid();
+    		break;
+    	case 2:
+    		submenuIos();
+    		break;
+    	default:
+    		System.out.println("CERRANDO APLICACION...");
+    		System.exit(0);
+    		break;
+    	}
+    	menuPrincipal();
+    }
+    
+  //Menu de la opcion Smartphone Android
+    public static void submenuAndroid() {
+    	final int tope=4;  //mirar si es 1 o 2 lo que hay q poner
+    	int opc;
+    	do {
+    		System.out.println("\n \n ===   SUBMENU SMARTPHONES ANDROID   === \n");
+    		System.out.println(" 1 - Alta de dispositivo");
+    		System.out.println(" 2 - Mostrar listado de dispositivos");
+    		System.out.println(" 3 - Examen");
+    		System.out.println(" 4 - Volver al menu principal");
+    		System.out.println("Introducir opcion: ");
+    		opc = sc.nextInt();
+    		if (opc <1 || opc > tope) {
+    			System.out.println("Error opcion invalida. Intentelo de nuevo");
+    		}
+    	}while (opc <1 || opc >tope);
+    	switchSubmenuAndroid(opc);
+    }
+    //Menu de la opcion sms
+    public static void submenuIos() {
+    	final int tope=4;
+    	int opc;
+    	do {
+    		System.out.println("\n \n ===   SUBMENU SMARTPHONES IOS  === \n");
+    		System.out.println(" 1 - Alta de dispositivo");
+    		System.out.println(" 2 - Mostrar listado de dispositivos ");
+    		System.out.println(" 3 - examen");
+    		System.out.println(" 4 - Volver al menu principal");
+    		System.out.println("Introducir opcion: ");
+    		opc = sc.nextInt();
+    		if (opc <1 || opc > tope) {
+    			System.out.println("Error opcion invalida. Intentelo de nuevo");
+    		}
+    	}while (opc <1 || opc >tope);
+    	switchSubmenuIos(opc);
+    }
+    
+    
+    
+  //Switch del menu de Smartphone Android
+    public static void switchSubmenuAndroid(int opcion) {
+    	switch (opcion) {
+	    	case 1:
+	    		Principal.AltaDispositivoAndroid();
+	    		break;
+	    	
+	    	case 2:
+	    		Principal.mostrarListadoAndroid();
+	    		break;
+	    	case 3:
+	    		Principal.Examen();
+	    		break;
+	    	case 4:
+	    		menuPrincipal();
+	    		break;
+    	}	
+    	submenuAndroid();
+    }
+  //Switch del menu de Smartphone IOS
+    public static void switchSubmenuIos(int opcion) {
+    	switch (opcion) {
+	    	case 1:
+	    		Principal.AltaDispositivoIos();
+	    		break;
+	    	case 2:
+	    		Principal.mostrarListadoIos();
+	    		break;
+	    	case 3:
+	    		Principal.Examen2();
+	    		break;
+	    	case 4:
+	    		menuPrincipal();
+	    		break;
+    	}	
+    	submenuIos();
+    }
+ 
 }
